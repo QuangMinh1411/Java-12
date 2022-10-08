74d4ff0a1944e56f9e4e0b793244ce85562ba2ba
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2022-10-08 14:23:45 +0700

    Product demo

---
+++ Day 14/product-demo/src/main/java/vn/techmaster/productdemo/model/Category.java
@@ -0,0 +1,15 @@
+package vn.techmaster.productdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private int id;
+    @NotBlank(message = "category not null")
+    private String name;
+}
---
+++ Day 14/product-demo/src/main/java/vn/techmaster/productdemo/service/StorageService.java
@@ -0,0 +1,54 @@
+package vn.techmaster.productdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import vn.techmaster.productdemo.exception.StorageException;
+import vn.techmaster.productdemo.model.Product;
+import vn.techmaster.productdemo.repostory.ProductRepository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+@Service
+public class StorageService {
+    @Autowired
+    private ProductRepository productRepo;
+    @Value("${upload.path}")
+    private String path;
+    public void uploadFile(MultipartFile file, int id) throws Exception {
+        if(file.isEmpty()){
+            throw new StorageException("Failed to store empty file");
+        }
+        String fileName = file.getOriginalFilename();
+
+        try{
+            var is = file.getInputStream();
+            var address = path + "pic"+id+".jpg";
+            Files.copy(is, Paths.get(address), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            var msg = String.format("Failed to store file %s",fileName);
+            throw new StorageException(msg,e);
+        }
+    }
+
+    public void deleteFile(int id){
+        Optional<Product> productOp = productRepo.get(id);
+        if(productOp.isPresent()){
+            Product product = productOp.get();
+            String fileName = product.getPhoto().getOriginalFilename();
+            try{
+                var address = path + "pic"+id+".jpg";
+                Files.deleteIfExists(Paths.get(address));
+            }catch(Exception e){
+                var msg = String.format("Failed to delete file %s",fileName);
+                throw new StorageException(msg,e);
+            }
+        }
+    }
+
+}
